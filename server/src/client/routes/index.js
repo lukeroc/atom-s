21dd920ba8285602f0715f7eef847a109f61103b
Luke Rocco <luke.rocco@globalgaming.com>
2017-11-23 11:45:49 +0100

    added new App route without path and moved existing routes as child routes of App

--- server/src/client/routes/index.js
+++ server/src/client/routes/index.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 
+import App from '../components/app'
 import Home from '../components/pages/Home'
 import Users from '../components/pages/Users'
 
 export default [
   {
-    ...Home,
-    path: '/',
-    exact: true
-  },
-  {
-    ...Users,
-    path: '/users',
-    exact: true
+    ...App,
+    routes: [
+      {
+        ...Home,
+        path: '/',
+        exact: true
+      },
+      {
+        ...Users,
+        path: '/users',
+        exact: true
+      }
+    ]
   }
 ]
