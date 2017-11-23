b7c5273ad6438924338733103f94ee188ab019c3
Luke Rocco <luke.rocco@globalgaming.com>
2017-11-23 11:39:48 +0100

    updated FETCH_USERS action to handle custom axios instances from client and server

--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -1,9 +1,12 @@
-import axios from 'axios'
-
 export const FETCH_USERS = 'FETCH_USERS'
 
-export const fetchUsers = () => async dispatch => {
-  const payload = await axios.get('http://react-ssr-api.herokuapp.com/users')
+export const fetchUsers = () => async (dispatch, getState, api) => {
+
+  // api is the server and client axios instane
+  // Used instead of axios directly to support SSR authenticaation
+  // By doing so, the same action creator can be used by client and server
+  // eventhough they have 2 separate configs
+  const payload = await api.get('/users')
 
   dispatch({
     type: FETCH_USERS,
